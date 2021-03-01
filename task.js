5e5f713079ffc31746b981798c2e11481869449f
Ilya Glazyrin <glazyrinilya2011@gmail.com>
2021-03-01 21:35:46 +0500

    done homework

--- task.js
+++ task.js
@@ -1,9 +1,28 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+    let S = amount - contribution; // тело кредита
+    let P = percent / 100 / 12; // 1/12 процентной ставки
+    let date1 = new Date()
+
+    let n = (date.getFullYear()*12 + date.getMonth()) - (date1.getFullYear()*12 + date1.getMonth());
+    let monthAmount = S * ( P + P / (((1 + P) ** n) - 1));
+    let totalAmount = monthAmount * n;
+
+    return totalAmount.toFixed(2);
+
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+    let greeting;
+
+    if (name === "" || name === null || name === undefined) {
+        greeting = "Привет, мир! Меня зовут Аноним.";
+    } else {
+        greeting = `Привет, мир! Меня зовут ${name}.`
+    }
+    
+    return greeting;
 }
